cea8f49f4484a030f0f32788c222c4d383b615d2
Gary11777 <iharushakou@gmail.com>
2025-03-08 12:10:24 +0300

    second

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class Post extends Model
+class Comment extends Model
 {
-    use Likable;
+   use Likable;
 }
